05ed62889d40cc0f78f1cd5280c7ed25403dc6c1
flavianoale <39352655+flavianoale@users.noreply.github.com>
2026-02-18 01:38:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Cole aqui:
 // - https://ianplhvvohxkhtgidweo.supabase.co
 // - sb_publishable_H2-iAuhGrGuA7-4Wxnt6zA_gtIPyTk
-window.VIDA_SUPABASE_URL = "COLE_AQUI_SEU_PROJECT_URL";
-window.VIDA_SUPABASE_ANON_KEY = "COLE_AQUI_SUA_ANON_PUBLIC_KEY";
+window.VIDA_SUPABASE_URL = "https://ianplhvvohxkhtgidweo.supabase.co";
+window.VIDA_SUPABASE_ANON_KEY = "sb_publishable_H2-iAuhGrGuA7-4Wxnt6zA_gtIPyTk";
